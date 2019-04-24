4d6d765df360e53475fdc7768aeec27ef28a56d9
Ubuntu <ubuntu@ip-172-31-53-68.ec2.internal>
2019-04-24 20:12:51 +0000

    done

--- logs-git.sh
+++ logs-git.sh
@@ -6,17 +6,22 @@ YELLOW='\e[93m'
 NC='\033[m'
 
 #### CREATING A TIMESTAMP VARIABLE FOR THE NAME OF THE OUTPUT FILE ####
-TIMESTAMP=`date +%F-%H:%M:%S`
+TIMESTAMP=`date +%F_%Hh-%Mm-%Ss`
 
 #### RUNNING ANSIBLE - LOG OUTPUT TO FILE ####
 echo -e "${YELLOW}Running Ansible with playbook $1 ... ${NC}"
+ansible-playbook $1 -v > "logs/ansible-output_${TIMESTAMP}.log"
+cat "logs/ansible-output_${TIMESTAMP}.log"
+echo -e "${YELLOW}ANSIBLE OUTPUT: ${GREEN}logs/ansible-output_${TIMESTAMP}.log${NC}"
 
-ansible-playbook $1
+#### COMPRESSING THE LOGS ####
+echo -e "${YELLOW}Compressing log files... ${NC}"
+tar -cvf logs/ansible-output_${TIMESTAMP}.tar logs/ansible-output_${TIMESTAMP}.log
 
 #### SEND LOGS TO REPO ####
 echo -e "${YELLOW}Sending logs to repository... ${NC}"
 git pull
-git add logs/*
+git add logs/ansible-output_${TIMESTAMP}.tar
 git commit -m "Ansible Output Logs from  ${TIMESTAMP}"
 git push
 
